22c64ef59d3e050e8179220fc7c26945cbb43cba
Abe Yang <abeyang@gmail.com>
2012-09-20 02:04:26 -0500

    Fixed #34

--- dev/aboutus.php
+++ dev/aboutus.php
@@ -2,70 +2,165 @@
 	$PAGE_ID = 'about';
 	$PAGE_TITLE = 'About Us';
 	require('_header.php'); 
-?>
-
-	<!-- START BODY -->
-	
+?><!-- START BODY -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
 		<div class="container">
 			<div class="row">
 				<div id="video-section" class="span12">
-					<!-- code for video will go in place of placeholder -->
-					<img src="http://placehold.it/1020x500" alt="">
-					<p>Come get to know us. We hope we'll get to know you, too!</p>
+					<div class="video">
+						<iframe src="http://player.vimeo.com/video/15676413?title=0&amp;byline=0&amp;portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+					</div>
+					<p>
+						Come get to know us. We hope we'll get to know you, too!
+					</p>
 				</div>
 			</div>
-		</div> <!-- /.container -->
+		</div><!-- /.container -->
 		<div class="nocontainer">
 			<div class="who-we-are">
 				<div id="ribbon">
-					<p>Who We Are</p>
+					<h3>
+						Who We Are
+					</h3>
 				</div>
-				<div id="carousel-banner">
-				</div>
-				<div id="who-we-are-content">
-					<div class="container">
-						<div class="row">
-								<div class="span12">
-							<p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus</p>
-								</div>
+				<div id="storyCarousel" class="carousel slide">
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+						<div class="active item">
+							<img src="http://farm5.staticflickr.com/4018/4684490158_3b5f5cfca3_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									We first came down from Berkeley on June 2010, and planted the church here in Riverside. Here we are with some helpers from Berkeley!
+								</p>
+							</div>
 						</div>
-					</div>
+						<div class="item">
+							<img src="http://farm7.staticflickr.com/6204/6122902708_8dddc29180_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									Steve and Eunice, the leaders of the Riverside church!
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm6.staticflickr.com/5007/5266638890_9163d2a1cc_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									We went back to Berkeley during Thanksgiving break, 2010, for our first church retreat!
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm5.staticflickr.com/4109/5450893573_2c7eee7853_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									In the past year, we had our first annual community service event, Valentine's Day of Compassion!
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm8.staticflickr.com/7017/6794178105_453dd8706b_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									We also held our first baptism service.
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm8.staticflickr.com/7107/6926896438_11719c1fef_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									Here we are during a trip to Yosemite.
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm8.staticflickr.com/7128/7005275096_baec112fbb_b.jpg" alt="">
+							<div class="carousel-caption">
+								<p>
+									Now, our church has grown from twenty members to over sixty members!
+								</p>
+							</div>
+						</div>
+					</div><!-- Carousel nav -->
+					<a class="carousel-control left" href="#storyCarousel" data-slide="prev">‹</a> <a class="carousel-control right" href="#storyCarousel" data-slide="next">›</a>
 				</div>
-			</div>  
+			</div>
 			<div class="what-to-expect">
-				<div id="expectCarousel" class="carousel slide">
-				  <!-- Carousel items -->
-				  <div class="carousel-inner">
-				    <div class="active item">
-				    	<img src="http://farm7.staticflickr.com/6095/6275463544_4f1a113bee_b.jpg" alt="" />
-						<div class="carousel-caption">
-							<h4>Sports</h4>
+				<h3>
+					- What to Expect -
+				</h3>
+				<div id="what-to-expectCarousel" class="carousel slide">
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+						<div class="active item">
+							<img src="http://farm7.staticflickr.com/6121/6202249861_b1dd0ccf44_b.jpg" alt="">
+							<div class="carousel-caption">
+								<h4 id="fittext">
+									Word of God
+								</h4>
+							</div>
 						</div>
-				    </div>
-				    <div class="item">
-				    	<img src="http://farm7.staticflickr.com/6060/6379451775_5b5f7e6fde_b.jpg" alt="" />
-						<div class="carousel-caption">
-							<h4>Outings</h4>
+						<div class=" item">
+							<img src="http://farm7.staticflickr.com/6095/6275463544_4f1a113bee_b.jpg" alt="">
+							<div class="carousel-caption">
+								<h4 id="fittext">
+									Sports
+								</h4>
+							</div>
 						</div>
-				    </div>
-				    <!-- <div class="item">
-				    	<!-- PUT MORE IMAGES HERE
-				    </div> -->
-				  </div>
-				  <!-- Carousel nav -->
-				  <a class="carousel-control left" href="#expectCarousel" data-slide="prev">&lsaquo;</a>
-				  <a class="carousel-control right" href="#expectCarousel" data-slide="next">&rsaquo;</a>
+						<div class="item">
+							<img src="http://farm7.staticflickr.com/6060/6379451775_5b5f7e6fde_b.jpg" alt="">
+							<div class="carousel-caption">
+								<h4 id="fittext">
+									Outings
+								</h4>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm8.staticflickr.com/7174/6699146347_9763540620_b.jpg" alt="">
+							<div class="carousel-caption">
+								<h4 id="fittext">
+									Fun
+								</h4>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm8.staticflickr.com/7207/7146609923_51999115fa_b.jpg" alt="">
+							<div class="carousel-caption">
+								<h4 id="fittext">
+									Food
+								</h4>
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://farm7.staticflickr.com/6032/6274733911_4de76cf568_b.jpg" alt="">
+							<div class="carousel-caption">
+								<h4 id="fittext">
+									Praise
+								</h4>
+							</div>
+						</div>
+					</div><!-- Carousel nav -->
+					<a class="carousel-control left" href="#what-to-expectCarousel" data-slide="prev">‹</a> <a class="carousel-control right" href="#what-to-expectCarousel" data-slide="next">›</a>
 				</div>
-				
-				
-				
 			</div>
 			<div class="joinus">
-				<p>COME JOIN US!</p>
-			<div id="innerimage">
-			</div>
+				<h3>
+					Come Join Us!
+				</h3>
+				<div id="innerimage"></div>
+			</div><!--END BODY -->
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript">
+</script> <script src="jquery.fittext.js" type="text/javascript">
+</script> <script type="text/javascript">
+	$("#fittext").fitText();
+			</script> <?php require('_footer.php'); ?>
 		</div>
-	
-		<!--END BODY -->
-
-<?php require('_footer.php'); ?>
+	</body>
+</html>
