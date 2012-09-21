30e57383c1bdac502d4f65114ffa777e3c60c356
Abe Yang <abeyang@abes-imac.(none)>
2012-09-21 09:50:05 -0700

    Fixed #47. Fixed #43
    included $dev_mode as well as google analytics

--- dev/_footer.php
+++ dev/_footer.php
@@ -15,6 +15,22 @@
 		
 	</div> <!-- /#outer -->
 
+	<?php if (!$DEV_MODE) { ?>
+	<script type="text/javascript">
+	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', '<?php echo $ANALYTICS_KEY; ?>']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
+	</script>
+	<?php } ?>
+
 	<script src="js/less-1.3.0.min.js" type="text/javascript"></script>
 	<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
 	<script src="js/jquery.fitvids.js" type="text/javascript"></script>
