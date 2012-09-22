ce8f2631a07e9e7efb6efc03056cec493d64cb2c
Abe Yang <abeyang@gmail.com>
2012-09-22 11:44:02 -0700

    Fixed #45

--- _footer.php
+++ _footer.php
@@ -16,6 +16,7 @@
 	</div> <!-- /#outer -->
 
 	<?php if (!$DEV_MODE) { ?>
+	<!-- Google Analytics -->
 	<script type="text/javascript">
 	
 	  var _gaq = _gaq || [];
@@ -28,6 +29,21 @@
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
 	
+	</script>
+	
+	<!-- Gaug.es Analytics -->
+	<script type="text/javascript">
+	  var _gauges = _gauges || [];
+	  (function() {
+	    var t   = document.createElement('script');
+	    t.type  = 'text/javascript';
+	    t.async = true;
+	    t.id    = 'gauges-tracker';
+	    t.setAttribute('data-site-id', '4ebc5cd1613f5d62c3000003');
+	    t.src = '//secure.gaug.es/track.js';
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(t, s);
+	  })();
 	</script>
 	<?php } ?>
 
