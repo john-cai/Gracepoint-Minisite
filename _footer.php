b190a1568e408f561f3f909dfe310469692f0f11
Abe Yang <abeyang@gmail.com>
2012-11-11 10:45:03 -0800

    fixed #76

--- _footer.php
+++ _footer.php
@@ -56,11 +56,6 @@
 	    s.parentNode.insertBefore(t, s);
 	  })();
 
-	  // Crazy Egg
-		setTimeout(function(){var a=document.createElement("script");
-		var b=document.getElementsByTagName("script")[0];
-		a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0011/7790.js?"+Math.floor(new Date().getTime()/3600000);
-		a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
 	</script>
 	<?php } ?>
 
